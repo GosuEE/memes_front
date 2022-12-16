85fb96016a362bfd853dc3baa2dbd47f8b99c1a8
이정민 <kkookk55@naver.com>
2022-12-16 20:38:26 +0900

    Header fix,not yet completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,13 @@
-import React from 'react';
+import Box from '@mui/material/Box';
 import { AppBar, Tabs, Tab } from '@material-ui/core';
-import styled from 'styled-components';
-import HomeIcon from '@mui/icons-material/Home';
-import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
+import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import HomeIcon from '@mui/icons-material/Home';
+import DriveFileRenameOutlineIcon from '@mui/icons-material/DriveFileRenameOutline';
+import React from 'react';
 
-const StyledAppBar = styled(AppBar)`
-  && {
-    background-color: rgba(24, 17, 213, 0.5);
-    color: #445b87;
-    font-size: 1rem;
-    font-weight: 800;
-    margin-top: 20px;
-  }
-`;
-
-const Header = () => {
+const Header2 = () => {
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
@@ -24,19 +15,17 @@ const Header = () => {
   };
 
   return (
-    <>
-      <StyledAppBar position="static">
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
         <Tabs value={value} onChange={handleChange} aria-label="Main Tabs">
           <Tab label="Logo" icon={<DriveFileRenameOutlineIcon />} />
           <Tab label="Home" icon={<HomeIcon />} />
-
-          <Tab label="Write" icon={<DriveFileRenameOutlineIcon />} />
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}></Typography>
           <Button color="inherit">Login</Button>
         </Tabs>
-      </StyledAppBar>
-    </>
+      </AppBar>
+    </Box>
   );
 };
 
-export default Header;
+export default Header2;
