9e58912c6907a4f6de2529403c5d3d26e170eb8f
이정민 <kkookk55@naver.com>
2022-12-16 20:16:47 +0900

    헤더,메인 작업(미완)

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import Header from '../components/Header';
+import List from '../components/List';
+import LoginHeader from '../components/LoginHeader';
+import Header2 from '../components/Header2';
 function Main() {
-  const a = 2;
-  const b = 34;
-
-  return <div>dsdsd</div>;
+  return (
+    <>
+      <LoginHeader />
+      <Header />
+      <Header2 />
+      <List />
+    </>
+  );
 }
 
 export default Main;
