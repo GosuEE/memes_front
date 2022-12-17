b536fab5988150056a8c1877e949f3d5be1b3720
이정민 <kkookk55@naver.com>
2022-12-17 22:44:13 +0900

    add SignIn page

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -4,6 +4,7 @@ import Main from '../pages/Main';
 import Header from '../components/Header';
 import App from '../App';
 import Detail from '../pages/Detail';
+import SignIn from '../pages/SignIn';
 
 function Router() {
   return (
@@ -12,6 +13,7 @@ function Router() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/detail" element={<Detail />} />
+        <Route path="/signin" element={<SignIn />} />
       </Routes>
     </BrowserRouter>
   );
